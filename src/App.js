2cb7c6ce708cc637fc410ad7a76ad9c33d1e6cb4
CarlosIgreda <cjigredarivas@gmail.com>
2023-05-30 20:08:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles/App.css';
+import Header from './components/Header';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
