628266219c0ddbb221e4dfdd219f05007a1292be
abiola <abiola.arobieke@gmail.com>
2023-05-31 15:50:40 -0400

    Add RocketList.js file to the project

---
+++ src/components/RocketList.js
@@ -0,0 +1,16 @@
+import { useSelector } from 'react-redux';
+import RocketItem from './RocketItem';
+
+const RocketList = () => {
+  const { rocketsItem } = useSelector((store) => store.rockets);
+
+  return (
+    <div>
+      {rocketsItem.map((rocket) => (
+        <RocketItem key={rocket.id} rocket={rocket} />
+      ))}
+    </div>
+  );
+};
+
+export default RocketList;
