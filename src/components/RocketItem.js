a11f36bd45086f37c2de89af44c97733326dabb6
abiola <abiola.arobieke@gmail.com>
2023-05-31 15:56:10 -0400

    Modify RocketItem.js file

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -1,10 +1,25 @@
-const RocketItem = () => {
-  console.log('hi');
-  return (
-    <div>
-      <h1>This is rocket Item</h1>
+import '../styles/Rockets.css';
+import PropTypes from 'prop-types';
+
+const RocketItem = ({ rocket }) => (
+  <div className="d-flex card">
+    <div className="md-4">
+      <div className="card-img">
+        <img className="rocket-img" src={rocket.flickr_images[0]} alt="" />
+      </div>
     </div>
-  );
+    <div className="md-8 card-details">
+      <h2>{rocket.name}</h2>
+      <p className="description">{rocket.description}</p>
+      <button type="button" className="btn-card">
+        Add Reservation
+      </button>
+    </div>
+  </div>
+);
+
+RocketItem.propTypes = {
+  rocket: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default RocketItem;
