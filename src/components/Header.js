6201e69e085149dddc4ec09c5260e6e28bf63c0d
CarlosIgreda <cjigredarivas@gmail.com>
2023-06-01 00:17:47 -0500

    Update components folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,20 @@ const Header = () => (
   <>
     <nav className="nav">
       <div className="name-logo-container">
-        <img className="logo" src="https://i.postimg.cc/x8xhGBMh/planet.png" alt="logo" />
+        <NavLink to="/">
+          <img className="logo" src="https://i.postimg.cc/x8xhGBMh/planet.png" alt="logo" />
+        </NavLink>
         <NavLink className="name-logo" to="/">Space Travelers&apos; Hub</NavLink>
       </div>
       <ul className="ul">
         <li className="li">
-          <NavLink className="navlink" to="/">Rockets</NavLink>
+          <NavLink activeclassname="active" className="navlink" to="/">Rockets</NavLink>
         </li>
         <li className="li">
-          <NavLink className="navlink missions-navlink" to="/missions">Missions</NavLink>
+          <NavLink activeclassname="active" className="navlink missions-navlink" to="/missions">Missions</NavLink>
         </li>
         <li className="li">
-          <NavLink className="navlink" to="/profile">My Profile</NavLink>
+          <NavLink activeclassname="active" className="navlink" to="/profile">My Profile</NavLink>
         </li>
       </ul>
     </nav>
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,29 @@
-const Missions = () => (
-  <>
-  </>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchAllMissions } from '../redux/missions/missionSlice';
+
+const Missions = () => {
+  const state = useSelector((state) => state);
+  const { missions } = state;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllMissions('https://api.spacexdata.com/v3/missions'));
+  }, [dispatch]);
+
+  return (
+    <>
+      <h2>Missions</h2>
+      <br />
+      {missions.data?.map((mission) => (
+        <div key={mission.mission_id}>
+          <h3>{mission.mission_name}</h3>
+          <p>{mission.description}</p>
+          <br />
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Missions;
