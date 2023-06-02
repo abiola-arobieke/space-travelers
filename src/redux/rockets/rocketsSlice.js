8ba03898c9f65fa289fd719d7b5068f617ef6b05
abiola <abiola.arobieke@gmail.com>
2023-06-02 10:46:20 -0400

    Modify rocketSlice.js file

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -32,7 +32,7 @@ const rocketsSlice = createSlice({
     },
     cancelReservation: (state, action) => {
       const rocketId = action.payload;
-      state.rocketsItem = state.rocketsItem.map((rocket) => (
+      state.rocketsItem = state.rocketsItem?.map((rocket) => (
         rocket.id === rocketId ? { ...rocket, reserved: false } : { ...rocket }));
     },
   },
