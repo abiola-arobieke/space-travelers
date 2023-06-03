226d5d95add297fefaafe465a9496088394f6485
CarlosIgreda <cjigredarivas@gmail.com>
2023-06-03 06:46:48 -0500

    Update components folder

--- src/components/MissionItem.js
+++ src/components/MissionItem.js
@@ -1,25 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission, leaveMission } from '../redux/missions/missionSlice';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Missions.css';
 
-const MissionItem = ({ mission }) => (
-  <tr className="row-cell">
-    <td className="mission-td mission-name">{mission.mission_name}</td>
-    <td className="mission-td mission-description">{mission.description}</td>
-    <td className="mission-td">
-      <button type="button" className="btn btn-secondary member-button">NOT A MEMBER</button>
-    </td>
-    <td className="mission-td">
-      <button type="button" className="btn btn-outline-secondary join-button">Join Mission</button>
-    </td>
-  </tr>
-);
+const MissionItem = ({ mission }) => {
+  const dispatch = useDispatch();
+
+  const handleJoinMission = () => {
+    dispatch(joinMission(mission.mission_id));
+  };
+
+  const handleLeaveMission = () => {
+    dispatch(leaveMission(mission.mission_id));
+  };
+
+  return (
+    <tr className="row-cell">
+      <td className="mission-td mission-name">{mission.mission_name}</td>
+      <td className="mission-td mission-description">{mission.description}</td>
+      <td className="mission-td mission-member">
+        {mission.reserved ? (
+          <button type="button" className="btn btn-primary active-button">Active Member</button>
+        ) : (
+          <button type="button" className="btn btn-secondary member-button">NOT A MEMBER</button>
+        )}
+      </td>
+      <td className="mission-td mission-join">
+        {mission.reserved ? (
+          <button
+            type="button"
+            className="btn btn-outline-danger leave-button"
+            onClick={handleLeaveMission}
+          >
+            Leave Mission
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-outline-secondary join-button"
+            onClick={handleJoinMission}
+          >
+            Join Mission
+          </button>
+        )}
+      </td>
+    </tr>
+  );
+};
 
 MissionItem.propTypes = {
   mission: PropTypes.shape({
+    mission_id: PropTypes.string.isRequired,
     mission_name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    reserved: PropTypes.bool.isRequired,
   }).isRequired,
 };
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,16 +3,23 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const { rocketsItem } = useSelector((store) => store.rockets);
+  const missions = useSelector((store) => store.missions) || [];
+  const joinedMissions = missions.data?.filter((mission) => mission.reserved);
 
   return (
     <div className="d-flex mt-1">
       <div className="md-6">
-        <h3 className="profile-title">Mission</h3>
+        <h3 className="profile-title">Missions</h3>
+        <ul className="list-container">
+          {joinedMissions?.map((mission) => (
+            <li key={mission.mission_id}>{mission.mission_name}</li>
+          ))}
+        </ul>
       </div>
       <div className="md-6 ms-1">
         <h3 className="profile-title">Rockets</h3>
         <ul className="list-container">
-          {rocketsItem?.filter((rocket) => rocket.reserved === true).map((reserved) => (
+          {rocketsItem?.filter((rocket) => rocket.reserved === true)?.map((reserved) => (
             <li key={reserved.id}>{reserved.name}</li>
           ))}
         </ul>
