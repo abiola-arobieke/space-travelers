aeb1a84717ad59d73e9a712da9793cabcb6b507d
CarlosIgreda <cjigredarivas@gmail.com>
2023-06-03 06:47:18 -0500

    Update missionSlice.js

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -5,13 +5,20 @@ export const fetchAllMissions = createAsyncThunk('fetch-all-missions', async (ap
   return response.json();
 });
 
+export const joinMission = createAsyncThunk('join-mission', async (missionId) => missionId);
+
+export const leaveMission = createAsyncThunk('leave-mission', async (missionId) => missionId);
+
 const missionSlice = createSlice({
   name: 'missions',
   initialState: { data: [], fetchStatus: '' },
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchAllMissions.fulfilled, (state, action) => {
-      state.data = action.payload;
+      state.data = action.payload?.map((mission) => ({
+        ...mission,
+        reserved: false,
+      }));
       state.fetchStatus = 'success';
     });
     builder.addCase(fetchAllMissions.pending, (state) => {
@@ -19,6 +26,16 @@ const missionSlice = createSlice({
     });
     builder.addCase(fetchAllMissions.rejected, (state) => {
       state.fetchStatus = 'error';
+    });
+    builder.addCase(joinMission.fulfilled, (state, action) => {
+      const missionId = action.payload;
+      state.data = state.data?.map((mission) => (mission.mission_id === missionId
+        ? { ...mission, reserved: true } : mission));
+    });
+    builder.addCase(leaveMission.fulfilled, (state, action) => {
+      const missionId = action.payload;
+      state.data = state.data?.map((mission) => (mission.mission_id === missionId
+        ? { ...mission, reserved: false } : mission));
     });
   },
 });
